216d50517a2f2cf967343a5b574d585798608c78
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-29 12:33:32 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV || 'development']);
+
+app.use(express.json())
+
+app.get('/movies', function (req, res) {
+    knex
+        .select('*')
+        .from('movies')
+        .then(data => res.status(200).json(data))
+        .catch(err =>
+            res.status(404).json({ message: 'The data you are looking for could not be found. Please try again.' }))
+})
+
+app.listen(port, () => {
+    console.log(`The server is running on ${port}`)
+})
